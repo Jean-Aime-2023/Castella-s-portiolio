7bdc83c2776b56e6bd153483e02ff8de169536fd
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-01-26 08:44:05 +0200

    commits

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import image from '../assets/images/Portfolio.png';
 import image2 from '../assets/images/Group 3443.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init({
+  offset: 120,
+  delay: 0, 
+  duration: 800, 
+  easing: 'ease',
+});
 
 const Examples = () => {
   return (
     <div className="flex flex-col 2xl:px-[20rem] py-[10rem] gap-[20rem] max-md:py-[3rem] max-md:gap-[10rem] items-center max-2xl:px-[4rem] max-md: text-white poppins">
       <div className="flex flex-row max-lg:flex-col items-center">
-        <div className="flex flex-col gap-8">
+        <div data-aos="fade-right" data-aos-offset="200" data-aos-delay="50" className="flex flex-col gap-8">
           <section>
             <p className="text-[#9857D3] text-[15px]">Featured Project</p>
             <p className="text-3xl text-[#CCD6F6]">Example Project</p>
@@ -121,7 +129,7 @@ const Examples = () => {
 
         <div className="absolute blur2 right-[28rem] top-[190em]"></div>
 
-        <div>
+        <div data-aos="fade-left" data-aos-offset="200" data-aos-delay="50">
           <img
             src={image2}
             alt=""
@@ -131,7 +139,7 @@ const Examples = () => {
       </div>
 
       <div className="flex flex-row max-md:flex-col items-center">
-        <div className="flex flex-col gap-8 order-2 max-md:order-1 text-end">
+        <div data-aos="fade-right" data-aos-offset="200" data-aos-delay="50" className="flex flex-col gap-8 order-2 max-md:order-1 text-end">
           <section>
             <p className="text-[#9857D3] text-[15px]">Featured Project</p>
             <p className="text-3xl text-[#CCD6F6]">Example Project</p>
@@ -246,7 +254,7 @@ const Examples = () => {
 
         <div className="absolute blur2 left-[10rem] bottom-[50rem]"></div>
 
-        <div className="order-1 max-md:order-2">
+        <div data-aos="fade-left" data-aos-offset="200" data-aos-delay="50" className="order-1 max-md:order-2">
           <img src={image} alt="" className="w-[90%]" />
         </div>
       </div>
